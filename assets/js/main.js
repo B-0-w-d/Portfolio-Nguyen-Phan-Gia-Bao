3997e5df5ba65d265f3ea09a000fca122c8ba861
B-0-w-d <bo.july.august@gmail.com>
2025-12-20 09:30:33 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,117 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+/*=============== change bg header ===============*/
+const scrollHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('scroll-header') 
+                       : header.classList.remove('scroll-header')
+}
+/*=============== HOME TYPED JS ===============*/
+const typedHome = new Typed('#home-typed', {
+    strings: ['Web Developer', 'Web Designer', 'Freelancer'],
+    typeSpeed: 80,
+    backSpeed: 40,
+    backDelay: 2000,
+    loop: true,
+    cursorChar: '_',
+})
+
+/*=============== ADD SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the scroll-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('shadow-header') 
+                       : header.classList.remove('shadow-header')
+}   
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== CONTACT EMAIL JS ===============*/ 
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{ 
+    e.preventDefault()
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_szmrjsl', 'template_0b01m3a', '#contact-form', 'DcHbI1TmotTJihPO6')
+    .then(() => {
+        // Show sent message
+        contactMessage.textContent = 'Message sent successfully ✅'
+        // Remove message after five seconds
+        setTimeout(() => {
+            contactMessage.textContent = ''
+        }, 5000);   
+    })
+}
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionsClass.classList.add('active-link')
+        }else{
+            sectionsClass.classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2000,
+    // reset: true /* Animation repeat */
+    delay: 400,
+    // reset: true /* Animation repeat */
+})
+sr.reveal(`.home__content, .resume__content:nth-child(1), footer__container`)
+sr.reveal(`.home__data, .resume__content:nth-child(2)`, {delay: 300, origin: 'bottom'})
+sr.reveal(`.home__img`, {delay: 500, origin: 'bottom'})
+sr.reveal(`.home__social`, {delay: 600, origin: 'left'})
+sr.reveal(`.about__img, .contact__box`, {origin: 'left'})
+sr.reveal(`.about__data, .contact__form`, {delay: 300, origin: 'right'})
+sr.reveal(`.skills__content, .portfolio__content, .testimonial__content, .footer__content`, {interval: 100})
+sr.reveal(`.projects__card`, {interval: 100})
